b378dba88361a3fa3132ff0c6dd24f9dbffe4ea9
Ethiopia12345 <101978426+mijaleta@users.noreply.github.com>
2024-02-13 10:23:30 +0300

    start
    This start of the project

---
+++ index.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const socketio = require('socket.io');
+const mongoose = require('mongoose');
+![m-gtihub](https://github.com/mijaleta/qrcode/assets/101978426/7ce35c9a-99c9-4c21-82c5-06949b5d955e)
